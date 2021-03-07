05a06f97503541696182e1d84c0167b00055718b
robisonja <jadenrobison8@gmail.com>
2021-03-06 17:23:58 -0700

    add more css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
+var userInputEl = document.querySelector("#user-input");
+
+
+
+//function to generate city weather
+
+//function to generate city hourly weather
+
+//function to generate search suggestions
+
+//save input
+
+//load saved data
+
 
